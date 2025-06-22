d03d74561f1f1c5ed0a5a390a096de69ca049406
Daniel Avelar <daniel.avelar@gowsolucoes.com>
2025-06-22 19:35:23 -0300

    Ajustes finos

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # GoProgramadoresGOW
+
+# COMMANDS
+$ docker compose down --volumes --remove-orphans # Remove os containers criados.
+$ docker compose build --no-cache # Cria imagem DOCKER
+$ docker compose up #Orquestra e monta os containers
--- db/db.go
+++ db/db.go
@@ -5,38 +5,40 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func Connect() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Erro ao carregar .env:", err)
-	}
-
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 
-	connStr := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
-	)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-	DB, err = sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Erro ao abrir conexão:", err)
+	var err error
+
+	for i := 0; i < 10; i++ {
+		DB, err = sql.Open("postgres", dsn)
+		if err != nil {
+			log.Printf("Erro ao abrir conexão: %v\n", err)
+		} else {
+			err = DB.Ping()
+			if err == nil {
+				fmt.Println("✅ Banco conectado")
+				return
+			}
+			log.Printf("Erro ao pingar DB: %v\n", err)
+		}
+		log.Println("Tentativa de conexão falhou, tentando novamente em 2 segundos...")
+		time.Sleep(2 * time.Second)
 	}
 
-	if err = DB.Ping(); err != nil {
-		log.Fatal("Erro ao pingar DB:", err)
-	}
-
-	fmt.Println("✅ Banco conectado")
+	log.Fatalf("Não foi possível conectar ao banco após várias tentativas: %v", err)
 }
